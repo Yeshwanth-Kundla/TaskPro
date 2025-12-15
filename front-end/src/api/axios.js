fbee95841bafc0b6a70173ed8684d2e7e99f59f6
Yeshwanth-Kundla <yeshwanthkundla@gmail.com>
2025-12-15 11:14:09 +0530

    Use env variable for backend URL

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api=axios.create({
-    baseURL:"https://taskpro-backend-ehhm.onrender.com",
+    baseURL:import.meta.env.VITE_API_URL,
     headers: { "Content-Type": "application/json" }
 });
 
